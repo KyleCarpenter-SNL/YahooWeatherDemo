104e45c37d619240069a6b22b6eab16065882d8b
Kyle <Kylecarpenter@sportsnowlive.com>
2017-12-10 21:49:12 -0500

    Edited spelling of labels

---
+++ Home.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Home : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    [System.Web.Services.WebMethod]
+    public static string setTXT(string city, string temp, string cond)
+    {
+        string fullTXT = "The temperature in " + city + " is " + temp + "°F and the current conditions are " + cond + ".";
+
+        return fullTXT;
+    }
+}
